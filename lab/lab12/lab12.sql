cc5a4a798bc78eb24f30a2ee56ac229aef64801d
Lin Xu <lllinx@umich.edu>
2018-03-11 20:12:11 -0500

    lab12 sql

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -2,16 +2,17 @@
 .read fa16data.sql
 
 CREATE TABLE obedience AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select seven,denero from students;
 
 CREATE TABLE smallest_int AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select time,smallest from students where smallest>8 order by smallest limit 20;
 
 CREATE TABLE greatstudents AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select a.date, a.number, a.pet, a.color, b.color from students a, sp16students b 
+  where a.date=b.date and a.number=b.number and a.pet=b.pet;
 
 CREATE TABLE sevens AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select a.seven from students a, checkboxes b where a.time=b.time and a.number==7 and b.'7'=='True';
 
 CREATE TABLE matchmaker AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select a.pet, a.song, a.color, b.color from students a, students b where a.time<b.time and a.pet=b.pet and a.song=b.song;
